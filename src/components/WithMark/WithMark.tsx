78c707e47cbe42740881673afb214a56711848d0
cpb-home <cpb@bizb.ru>
2024-04-21 13:09:42 +0300

    updated to FC

--- src/components/WithMark/WithMark.tsx
+++ src/components/WithMark/WithMark.tsx
@@ -4,6 +4,36 @@ import Popular from "../Popular/Popular";
 import { IArticle } from "../Article/Article";
 
 export default function WithMark(Component) {
+  return (props: IArticle) => {
+    
+    if (props.views < 100) {
+      return (
+        <New>
+          <Component {...props} />
+        </New>
+      )
+    } else if (props.views > 1000) {
+      return (
+        <Popular>
+          <Component {...props} />
+        </Popular>
+      )
+    } else {
+      return (
+        <Component {...props} />
+      )
+    }
+  }
+}
+
+
+/*
+import React from "react";
+import New from "../New/New";
+import Popular from "../Popular/Popular";
+import { IArticle } from "../Article/Article";
+
+export default function WithMark(Component) {
   return class extends React.Component {
     render() {
       const propsItems: IArticle = this.props;
@@ -28,3 +58,5 @@ export default function WithMark(Component) {
     }
   }
 }
+
+*/
